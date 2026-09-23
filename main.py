50e756332746d08e6d83b0104f2a7e185a6e122e
GHIFARI <rizkialghifari2008@gmail.com>
2026-09-23 11:56:56 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("=== Kalkulator Terminal Modul 3 ===")
+angka1 = float(input("Masukkan angka pertama: "))
+angka2 = float(input("Masukkan angka kedua: "))
+hasil = angka1 + angka2
+
+print(f"Status: Setup Berhasil!")
+print(f"Hasil Penjumlahan: {hasil}")
